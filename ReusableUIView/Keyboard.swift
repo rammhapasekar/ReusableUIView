4f92341a0946ae31570a25cd9c325d595ffae0e3
sid2792 <sid.2792@gmail.com>
2017-03-07 12:12:49 +0530

    Initial Commit

---
+++ ReusableUIView/Keyboard.swift
@@ -0,0 +1,21 @@
+//
+//  Keyboard.swift
+//  ReusableUIView
+//
+//  Created by Admin on 07/03/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class Keyboard: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
